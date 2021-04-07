b5a5a085454c1126211abf137feebd6a1cbb88f7
Esben Viskum <viskum@viskum.dk>
2021-04-07 14:11:58 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Esben Viskum on 07/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
